1ec46e73328e12f5049064a9f60ab593be7181e3
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-05-09 14:55:48 -0300

    feat: adicionando servico de login

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { loginResponse } from '../types/login-response.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient) { 
+
+  }
+
+  login(name: string, password: string) {
+    return this.httpClient.post<loginResponse>("/login", {name, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+}
